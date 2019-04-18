8b89c7bac9b8f754e85010ce7eef1f599271b9d8
liangh <liangh1009@gmail.com>
2019-04-19 01:48:25 +0800

    comment added for D

--- include/Diff.h
+++ include/Diff.h
@@ -171,7 +171,9 @@ namespace Diff {
 	// make a Expression object reprent the differential respect` s`
 	// `var` must be handler of Variable object
 	Expr D(Expr const &expr, Expr const &var);
+	// i'th times differential
 	Expr D(Expr const &expr, Expr const &var, int i);
+	// pair.second'th times differential w.r.t pair.first
 	Expr D(Expr const &expr, std::pair<Expr, int> const &pair);
 
 	Expr Integrate(ExprOrDouble const &y, Expr const &x, ExprOrDouble const &from, ExprOrDouble const &to);
